7d40562f15db7b7123d3ebbe3c75cb84006af53c
Samie Al Fauzan <samiealfauzan184@gmail.com>
2026-08-12 20:33:38 +0700

    Fix proxy trust for Railway

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Baris ini memberitahu Laravel untuk mempercayai HTTPS dari Railway
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
